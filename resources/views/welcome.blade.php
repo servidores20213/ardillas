67c6167b03b7747f04a59aa0492ad59996248f43
servidores20213 <servidoresc@slp.tecnm.mx>
2022-05-12 17:43:29 -0500

    Modificación 12-05-22

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,8 @@
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-                    <h1>Administraci&oacute;n de Servidores 2022</h1>
+                    <h1>Administraci&oacute;n de Servidores 2022</h1><br>
+                    <h2> Modificación para GitHub </h2>
                 </div>
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
